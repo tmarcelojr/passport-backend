d77729ddae1e10b0f2a78a0648c68422c0749d6c
Teo Marcelo <tmarcelojr@gmail.com>
2020-12-11 09:12:13 -0600

    test

--- server.js
+++ server.js
@@ -39,14 +39,22 @@ mongoose.connect(
 // 	})
 // );
 
+const whitelist = ['http://localhost:4000', 'https://c-passport.herokuapp.com']; //white list consumers
 const corsOptions = {
-	origin: 'https://c-passport.herokuapp.com/',
-	methods: ['GET', 'POST'],
-	allowedHeaders: ['Origin','X-Requested-With','contentType','Content-Type','Accept','Authorization'],
-	credentials: true,
-	optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
+app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
 
 // Middleware
 app.use(bodyParser.json());
