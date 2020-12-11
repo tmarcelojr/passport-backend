1efce8abec74d3150c7fb5186e5b0202d68a6f72
Teo Marcelo <tmarcelojr@gmail.com>
2020-12-11 08:18:26 -0600

    initial commit

---
+++ passportConfig.js
@@ -0,0 +1,46 @@
+const User = require('./user')
+const bcrypt = require('bcryptjs')
+const localStrategy = require('passport-local').Strategy
+
+module.exports = function(passport) {
+  // we are getting a param passed when we call it, going to be the passport library to use the same instance of passport throughout entire app
+  passport.use(
+    // defining local strategy for passport everytime we use local strategy its going to run the local strategy we are going to define
+    new localStrategy((username, password, done) => {
+      // username and password are from our req.body
+      // done is callback function that works like .then()
+      User.findOne({username: username}, (err, user) => {
+        if (err) throw err
+        // if no user return null and no user
+        if (!user) return done(null, false)
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) throw err
+          if (result === true) {
+            return done(null, user)
+          } else {
+            return done(null, false)
+          }
+        })
+      })
+    })
+  )
+
+
+  // passport requires serialize and deserialize User
+  // serialize stores cookie inside browser
+  // take user from local strategy we created and create cookie with the user id
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id)
+  })
+
+  // deserialize takes the cookie and unravels it and returns a user from it
+  // find user from database that matches cookie id
+  passport.deserializeUser((id, cb) => {
+    User.findOne({ _id: id}, (err, user) => {
+      cb(err, user)
+    })
+  })
+}
+
+//https://www.youtube.com/watch?v=IUw_TgRhTBE
---
+++ server.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const passportlocal = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const { deserializeUser } = require('passport');
+const app = express();
+const User = require('./user');
+
+
+let mongoURI = "";
+
+if (process.env.NODE_ENV === "production") {
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/apassport";
+}
+
+
+
+mongoose.connect(
+	mongoURI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	() => {
+		console.log('Mongoose is connected');
+	}
+);
+
+// Need to set up CORS like this for auth to work
+app.use(
+	cors({
+		origin: 'http://localhost:3001',
+		credentials: true
+	})
+);
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	session({
+		// We will use secret in our cookie-parser
+		secret: 'this will be our secret code',
+		resave: true,
+		saveUninitialized: true
+	})
+);
+
+// Pass in the actual value of secret
+app.use(cookieParser('this will be our secret code'));
+app.use(passport.initialize())
+app.use(passport.session())
+require('./passportConfig')(passport)
+
+
+// Routes
+app.post('/login', (req, res, next) => {
+  // use local strategy we defined
+  passport.authenticate('local', (err, user, info) => {
+    if (err) throw err
+    if (!user) res.send('No User Exists')
+    else {
+      req.login(user, err => {
+        if (err) throw err
+        res.send('Successfully Authenticated')
+        console.log(req.user)
+      })
+    }
+  })(req, res, next)
+});
+
+app.post('/register', (req, res) => {
+	User.findOne({ username: req.body.username }, async (err, doc) => {
+		if (err) throw err;
+		if (doc) res.send('User Already Exists');
+		if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+			const newUser = new User({
+				username: req.body.username,
+				password: hashedPassword
+			});
+			await newUser.save();
+			res.send('User Created');
+		}
+	});
+});
+
+// req.user stores the user
+// req object will not be a user object containing session data
+// accessible throughout whole app
+app.get('/getUser', (req, res) => res.send(req.user));
+
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
