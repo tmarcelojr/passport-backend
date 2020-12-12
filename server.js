397b2b890f55542c910dbd882a1f1c801a082766
Teo Marcelo <tmarcelojr@gmail.com>
2020-12-11 22:46:09 -0600

    fixing deployed getUser() func

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post('/register', (req, res, next) => {
 // req object will not be a user object containing session data
 // accessible throughout whole app
 app.get('/user', (req, res) => {
-	console.log('req', req)
+	console.log('req', req.sessions)
 	// console.log('user', req.user);
 	// res.json({data: 'hi', user: req.user})
 	// if(req.user) {
